e11fc864c8ab58b6fd9933673dc83844a4d14c11
Yenner Alayon <yenneralayon@hotmail.com>
2024-02-18 19:56:02 -0500

    feat(back): Add new Solutions}

--- 03_string.py
+++ 03_string.py
@@ -1,3 +1,4 @@
+"""
 my_string = 'Mi String'
 my_other_string = 'Mi otro String'
 
@@ -11,4 +12,61 @@ print(my_new_line_string)
 
 my_tab_string = "Este es un string con tabulacion"
 print(my_new_line_string)
+
+# Invertir cadena
+
+cadena = "esto es una cadena"
+result = ""
+
+result = cadena[::-1]
+print(result)
+
+
+#Contar un  caracter
+
+cadena = "LLLLLss"
+letra = 'L'
+count = 0
+
+for i in cadena:
+    if i == letra:
+        count += 1
+
+print(count) 
+
+
+# Distancia de Hamming
+
+cadena1 = "esto es futbol"
+cadena2 = "esto es futdolzxcsd"
+count = 0
+
+if len(cadena1) != len(cadena2):
+    raise Exception("Las longitudes son distintas")
+
+for i in range(len(cadena1)):
+    if cadena1[i] != cadena2[i]:
+        count += 1
+
+print(count)
+
+
+#Contar palabras
+
+cadena = "Esto es futbol"
+
+palabras = cadena.split()
+
+count_Palabras = len(palabras)
+
+print(count_Palabras)
+ """
+
+
+cadena = "danna es sapa"
+result = ""
+
+result = cadena[::-1]
+
+print(result)
 
--- 04_lists.py
+++ 04_lists.py
@@ -11,6 +11,22 @@ print(type(my_list))
 my_other_list = [35,1.77, "Brairs","Moure"]
 print(type(my_other_list))
 
+## Arreglo Bidimensional
 
+def hourglassSum(arr):
+    # Write your code here
+  maxSum = -99
+  
+  for i in range(4):
+     for j in range(4):
+        top = sum(arr[i][j:j+3])
+        mid = arr[i + 1][j + 1]
+        bot = sum(arr[i + 2][j:j+3])
+        
+        hourGlass = top + mid + bot
+        maxSum = max(hourGlass,maxSum) 
+        
+         
+  return maxSum
 
 
